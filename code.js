f56929791fb832d8836789b3a7f194c09c5a8260
Roopa <roopatel@valdosta.edu>
2019-08-22 20:30:30 -0400

    ALL THE PAGES ARE LINKED YAYYYY

--- code.js
+++ code.js
@@ -25,11 +25,16 @@ var reserve = [
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
-  });
+});
 
-  app.get("/table", function(req, res) {
+app.get("/table", function(req, res) {
     res.sendFile(path.join(__dirname, "table.html"));
-  });
+});
+  
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
 
 app.get("/api/reservations", function(req, res) {
     return res.json(reserve);
